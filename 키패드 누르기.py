1575ddb0ec8a3a7dcea46480960650afef9f3ff6
pung <positionace@naver.com>
2022-07-21 21:01:31 +0900

    if문으로 모든 경우를 직접 작성하여서 좋지못한 코드지만 일단 성공은 했다!

--- 키패드 누르기.py
+++ 키패드 누르기.py
@@ -1,3 +1,20 @@
+"""
+ 1   2   3                
+ 4   5   6
+ 7   8   9
+10  11 12
+leftcen으로 나올 수 있는 값
+왼손이 1이라 가정
+1,4,7,10
+왼손이 4 라 가정
+2,1,4,7
+왼손이 7이라 가정
+1,2,
+
+
+
+rightcen으로 나올 수 있는 값
+"""
 def solution(numbers, hand):
     left=[1,4,7,10] # 왼손으로 눌러야 하는 애들
     right=[3,6,9,12] # 오른손으로 눌러야 하는 애들
@@ -20,38 +37,160 @@ def solution(numbers, hand):
             answer=answer+click
             righthand=i
         elif i in center:
-            rcenter=abs(righthand-i)
-            lcenter=abs(lefthand-i)
-            if righthand in center:
-                if abs(righthand-i)==3:
-                    rcenter=1
-                elif abs(righthand-i)==6:
-                    rcenter=2
-                elif abs(righthand-i)==9:
-                    rcenter=3
-            if lefthand in center:
-                if abs(lefthand-i)==3:
-                    lcenter=1
-                elif abs(lefthand-i)==6:
-                    lcenter=2
-                elif abs(lefthand-i)==9:
-                    lcenter=3
-            if rcenter==2 and lcenter==4:  #2 차 보안
-                rcenter=0# 같은 거리 만들어주기
-                lcenter=0# 같은 거리 만들어주기
-            if rcenter==4 and lcenter==2:
-                rcenter=0
-                lcenter=0        
-            if rcenter>lcenter:
+            if lefthand==1:
+                if i==2:
+                    leftcen=1
+                elif i==5:
+                    leftcen=2
+                elif i==8:
+                    leftcen=3
+                elif i==11:
+                    leftcen=4
+            elif lefthand==2:
+                if i==2:
+                    leftcen=0
+                elif i==5:
+                    leftcen=1
+                elif i==8:
+                    leftcen=2
+                elif i==11:
+                    leftcen=3
+            elif lefthand==4:
+                if i==2:
+                    leftcen=2
+                elif i==5:
+                    leftcen=1
+                elif i==8:
+                    leftcen=2
+                elif i==11:
+                    leftcen=3
+            elif lefthand==5:
+                if i==2:
+                    leftcen=1
+                elif i==5:
+                    leftcen=0
+                elif i==8:
+                    leftcen=1
+                elif i==11:
+                    leftcen=2
+            elif lefthand==7:
+                if i==2:
+                    leftcen=3
+                elif i==5:
+                    leftcen=2
+                elif i==8:
+                    leftcen=1
+                elif i==11:
+                    leftcen=2
+            elif lefthand==8:
+                if i==2:
+                    leftcen=2
+                elif i==5:
+                    leftcen=1
+                elif i==8:
+                    leftcen=0
+                elif i==11:
+                    leftcen=0
+            elif lefthand==10:
+                if i==2:
+                    leftcen=4
+                elif i==5:
+                    leftcen=3
+                elif i==8:
+                    leftcen=2
+                elif i==11:
+                    leftcen=1
+            elif lefthand==11:
+                if i==2:
+                    leftcen=3
+                elif i==5:
+                    leftcen=2
+                elif i==8:
+                    leftcen=1
+                elif i==11:
+                    leftcen=0
+            if righthand==3:
+                if i==2:
+                    rightcen=1
+                elif i==5:
+                    rightcen=2
+                elif i==8:
+                    rightcen=3
+                elif i==11:
+                    rightcen=4
+            elif righthand==2:
+                if i==2:
+                    rightcen=0
+                elif i==5:
+                    rightcen=1
+                elif i==8:
+                    rightcen=2
+                elif i==11:
+                    rightcen=3
+            elif righthand==6:
+                if i==2:
+                    rightcen=2
+                elif i==5:
+                    rightcen=1
+                elif i==8:
+                    rightcen=2
+                elif i==11:
+                    rightcen=3
+            elif righthand==5:
+                if i==2:
+                    rightcen=1
+                elif i==5:
+                    rightcen=0
+                elif i==8:
+                    rightcen=1
+                elif i==11:
+                    rightcen=2
+            elif righthand==9:
+                if i==2:
+                    rightcen=3
+                elif i==5:
+                    rightcen=2
+                elif i==8:
+                    rightcen=1
+                elif i==11:
+                    rightcen=2
+            elif righthand==8:
+                if i==2:
+                    rightcen=2
+                elif i==5:
+                    rightcen=1
+                elif i==8:
+                    rightcen=0
+                elif i==11:
+                    rightcen=0
+            elif righthand==12:
+                if i==2:
+                    rightcen=4
+                elif i==5:
+                    rightcen=3
+                elif i==8:
+                    rightcen=2
+                elif i==11:
+                    rightcen=1
+            elif righthand==11:
+                if i==2:
+                    rightcen=3
+                elif i==5:
+                    rightcen=2
+                elif i==8:
+                    rightcen=1
+                elif i==11:
+                    rightcen=0
+            if rightcen>leftcen:
+                lefthand=i
                 click="L"
                 answer=answer+click
-                lefthand=i
-            elif rcenter<lcenter:
+            elif leftcen>rightcen:
+                righthand=i
                 click="R"
                 answer=answer+click
-                righthand=i
             else:
-                if hand=='left':
+                if hand=="left":
                     lefthand=i
                     click="L"
                     answer=answer+click
