c3194786241e0c776202453e83ac58e5478c74b6
pung <positionace@naver.com>
2022-08-01 21:56:13 +0900

    그리디 알고리즘 문제인대 꽤 애먹었다. 먼저 정렬 해줘야 한 다는 것을 나는 처음에 생각하지 못했었고 예전에 뽑기문제처럼 리스트 삭제 대신에 필요없는값 대입해줬고 계속 안풀리다가 a리스트랑 b리스트 서로 같은 값을 가지고 있는거 처리하는 과정을 처음에는 for i in a: if i in b 제거 이런식으로 했었는대 생각해보니까 a리스트에서 b에 속하지 않은 것만 뺴오는 방식으로 하면 되는구나 싶어서 a2= [i for i in a if i not in b] b2= [i for i in b if i not in a] 이런식으로 정리하니까 해결되었음

---
+++ 체육복.py
@@ -0,0 +1,40 @@
+def solution(n, lost, reserve):
+   
+    reserve=sorted(list(set(reserve)))
+    lost=sorted(list(set(lost)))
+    lost2 = [i for i in lost if i not in reserve]
+    reserve2 = [i for i in reserve if i not in lost]
+    m=len(reserve2)
+    t=len(lost2)
+
+    c=n-t
+    for i in range(m):
+        for l in range(t):
+            if lost2[l]-1==reserve2[i] or lost2[l]+1==reserve2[i]:
+                c=c+1
+                lost2[l]=-1
+                reserve2[i]=-1
+    answer = c
+    return answer
+n=5
+lost=[2,4,5]
+reserve=[1,5,3]
+
+reserve=sorted(list(set(reserve)))
+lost=sorted(list(set(lost)))
+lost2 = [i for i in lost if i not in reserve]
+reserve2 = [i for i in reserve if i not in lost]    
+m=len(reserve2)
+t=len(lost2)
+print(reserve2)
+print(lost2)
+c=n-t
+for i in range(m):  
+    for l in range(t):
+        if lost[l]-1==reserve[i] or lost[l]+1==reserve[i]:
+            c=c+1
+            lost[l]=-5
+            reserve[i]=-9
+print(c)
+            
+
