87c8a2cab83bb0e36baf97cfa73d0aac9928cc57
pung <positionace@naver.com>
2022-08-07 19:00:15 +0900

    88.9 점 맞고 실패햇다 다음에 다른 문제 다 풀고 다시 도전해보기로 하였다

--- 실패율.py
+++ 실패율.py
@@ -11,49 +11,49 @@ import collections
 def solution(N, stages): 
     
     answer_1={}
-    answer=[] 
+    answer=[]
+    stages_len=len(stages) 
     stages_count=collections.Counter(stages)
     stages_count=dict(collections.OrderedDict(sorted(stages_count.items())))
+    stages_stack=0
     stages_clear={}
-    for i in stages:
-        counter=0
-        for l in stages:
-            if i<=l:
-                counter=counter+1
-        stages_clear[i]=counter
+    for i in stages_count:
+        if stages_len-stages_count[i]==0:
+            stages_clear[i]=-1
+        else:
+            try:
+                stages_stack=stages_stack+stages_count[i-1]    
+                stages_clear[i]=stages_len-stages_stack
+            except:
+                stages_clear[i]=stages_len
     stages_clear=dict(sorted(stages_clear.items()))
     temp=[]
     temp2=[]
-    for i in range(N):
-        temp.append(i+1)
-    temp_key=[]
-    temp_values=[]
-    temp_key2=[]
-    temp_values2=[]
+
+    # if N+1 in list(stages_count.keys()):
+    #     stages_count.pop(N+1)
+    #     stages_clear.pop(N+1)
+        
 
     i=0
     if N!=len(stages_count):
+        for i in range(N):
+            temp.append(i+1)
+        for k in stages_count.keys():
+            if k > N:
+                k=k
+            else: 
+                if k in stages_clear.keys():
+                
+                    answer_1[k]=stages_count[k]/stages_clear[k]
         
-        for i,l in stages_count.items():
-            if i>N:
-                i=i
-            else:
-                temp_key.append(i)
-                temp_values.append(l)
-        for i,l in stages_clear.items():
-            if i>N:
-                i=i
-            else:
-                temp_key2.append(i)
-                temp_values2.append(l)
+                
+        answer_1=sorted(answer_1,key=lambda x: answer_1[x], reverse=True)
         
-        for i in range(len(temp_key)):
-            answer_1[(temp_key[i])]=temp_values[i]/temp_values2[i]
-        answer_1=sorted(answer_1, key= lambda x : answer_1[x],reverse=True)
         temp2=[i for i in temp if i not in answer_1]
 
         answer=answer_1+temp2
-        
+        return answer
         
     else:
         for i in range(len(stages_clear)):
@@ -63,10 +63,8 @@ def solution(N, stages):
             except:
                 answer_1[i+1]=N/20000
 
-            answer= sorted(answer_1, key= lambda x : answer_1[x],reverse=True)
-            
-    return answer        
-        
+        answer= sorted(answer_1, key= lambda x : answer_1[x],reverse=True)
+        return answer    
 
 #----------------------------------------------------------------------
 
@@ -78,58 +76,65 @@ answer_1={}
 answer=[]
 # N=5
 # stages=[2, 1, 2, 6, 2, 4, 3, 3]
-N=4
-stages=[4,4,4,3,3,5]        
+N=8
+stages=[1,2,3,4,5,6,7]        
+stages_len=len(stages)
 stages_count=collections.Counter(stages)
 stages_count=dict(collections.OrderedDict(sorted(stages_count.items())))
+print(stages_count)
+stages_stack=0
 stages_clear={}
-for i in stages:
-    counter=0
-    for l in stages:
-        if i<=l:
-            counter=counter+1
-    stages_clear[i]=counter
+for i in stages_count:
+    if stages_len-stages_count[i]==0:
+        stages_clear[i]=-1
+    else:
+        try:
+            stages_stack=stages_stack+stages_count[i-1]    
+            stages_clear[i]=stages_len-stages_stack
+        except:
+            stages_clear[i]=stages_len
 stages_clear=dict(sorted(stages_clear.items()))
+print(stages_clear)
+print(stages_count)
 temp=[]
 temp2=[]
-for i in range(N):
-    temp.append(i+1)
-temp_key=[]
-temp_values=[]
-temp_key2=[]
-temp_values2=[]
+
+# if N+1 in list(stages_count.keys()):
+#     stages_count.pop(N+1)
+#     stages_clear.pop(N+1)
+    
 
 i=0
 if N!=len(stages_count):
+    for i in range(N):
+            temp.append(i+1)
+        
+    for k in stages_count.keys():
+        if k > N or k<1:
+            k=k
+           
+        else: 
+            if k in stages_clear.keys():
+            
+                answer_1[k]=stages_count[k]/stages_clear[k]
     
-    for i,l in stages_count.items():
-        if i>N:
-            i=i
-        else:
-            temp_key.append(i)
-            temp_values.append(l)
-    for i,l in stages_clear.items():
-        if i>N:
-            i=i
-        else:
-            temp_key2.append(i)
-            temp_values2.append(l)
+    answer_1=sorted(answer_1,key=lambda x: answer_1[x], reverse=True)
     
-    for i in range(len(temp_key)):
-        answer_1[(temp_key[i])]=temp_values[i]/temp_values2[i]
-    answer_1=sorted(answer_1, key= lambda x : answer_1[x],reverse=True)
     temp2=[i for i in temp if i not in answer_1]
-
+    if temp2[-1]>=N:
+        temp2.pop()
+    
     answer=answer_1+temp2
     print(answer)
-    
+    print("1")
 else:
+    
     for i in range(len(stages_clear)):
 
         try:
             answer_1[i+1]=stages_count[i+1]/stages_clear[i+1]
         except:
             answer_1[i+1]=N/20000
-
-        answer= sorted(answer_1, key= lambda x : answer_1[x],reverse=True)
-        print(answer)
+    print("2")
+    answer= sorted(answer_1, key= lambda x : answer_1[x],reverse=True)
+    print(answer)
