e141e12b2aad0413db89f240c8dad73a1a8a7a23
pung <positionace@naver.com>
2022-08-16 13:22:57 +0900

    기본문제

---
+++ 나누어 떨어지는 값.py
@@ -0,0 +1,19 @@
+def solution(arr, divisor):
+    answer=[]
+    for i in arr:
+        if i%divisor==0:
+            answer.append(i)
+    
+    if len(answer)==0:
+        return [-1]        
+    answer=sorted(answer)
+
+    return answer
+
+arr=[5, 9, 7, 10]
+divisor=5
+answer=[]
+for i in arr:
+    if i%divisor==0:
+        answer.append(i)
+
